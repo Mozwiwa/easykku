950fff6730d223e0ee8335d8c365941fdd0078e8
MOSDOTCOM <watcharaporns@kkumail.com>
2016-11-12 12:05:18 +0700

    Intent to SignUpActivity

--- app/src/main/java/com/kku/mosdotcom/easykku/MainActivity.java
+++ app/src/main/java/com/kku/mosdotcom/easykku/MainActivity.java
@@ -1,13 +1,40 @@
 package com.kku.mosdotcom.easykku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {// Main class
+
+//Explicit ประกาศตัวแปร
+    private Button signInButton, signUpButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button2);
+
+
+        //Sign Up Controller
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+
+
+
+
+
+    }//Main Method
+
+}//Main Class
---
+++ app/src/main/java/com/kku/mosdotcom/easykku/SignUpActivity.java
@@ -0,0 +1,8 @@
+package com.kku.mosdotcom.easykku;
+
+/**
+ * Created by MOSDOTCOM on 11/12/2016.
+ */
+public class SignUpActivity {
+
+}
